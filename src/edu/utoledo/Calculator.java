f46b3f1a98d59920a538e974e8f9cc90a871d354
Raya Smidi <rayasmidi@MacBook-Pro-6.local>
2022-11-18 16:05:58 -0500

    Initial commit

---
+++ src/edu/utoledo/Calculator.java
@@ -0,0 +1,2 @@
+package edu.utoledo;public class Calculator {
+}
---
+++ src/edu/utoledo/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Unit Test demo");
+        System.out.println(new Calculator().addTwoNumber(2, 2));
+    }
+}
